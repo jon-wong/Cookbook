2db96490126b55fdfa01a4c43c3f22962d7ee3c7
Jon Wong <jon.wongdc@gmail.com>
2016-12-05 16:17:18 -0600

    First commit of printing, inputs and vars

---
+++ printInputVar.py
@@ -0,0 +1,8 @@
+
+print("Printing and testing")
+a = input("What is your favorite color? ")
+print("My favorite color is " + a)
+print(type(a))
+b = input("What is the air speed velocity of a swallow? ")
+print(type(b))
+print("The air speed velocity of a sparrow is " + b)
